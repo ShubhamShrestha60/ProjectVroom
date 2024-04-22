5d7246f4e1bf3d1791340205310ce5a89353cf52
Bipaswi Shakya <106724004+Solvedsplash@users.noreply.github.com>
2024-04-22 22:11:50 +0545

    changes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,47 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import React, { useEffect,useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './components/home';
+import Login from './components/login';
+import Signup from './components/signup';
+import { Navbar } from "./components/Navbar";
+import { Footer } from "./components/footer";
+import Sidebar from "./components/Sidebar/Sidebar";
+import TopNav from "./components/TopNav/TopNav";
+import Booking from "./pages/Bookings";
+import Settings from "./pages/Settings";
+import Dashboard from "./pages/Dashboard";
+import AddCar from "./pages/addCar";
+import AdminLogin from "./admin/adminLogin";
+import AdminSignup from "./admin/adminSignup";
+import Cars from "./components/SearchResults/cars";
 
-// import Login from './components/Login&Signup/login'
-// import Signup from './components/Login&Signup/signup'
-import React, { useState } from 'react';
-import Home from './components/home/home'
-import Navbar from './components/Header/Navbar'
-import Footer from './components/footer/footer'
-import Cars from "./components/SearchResults/cars"
 
 export default function App() {
   const [results, setResults] = useState([]);
   return (
     <div>
-    <BrowserRouter>
-    <Navbar/>
-    <Routes>
-    
-     <Route path="/" element = {<Home setResults={setResults} results={results} />}/>
-     <Route
-          path="/cars"
-          element={<Cars results={results} />}
-        />
-
-    </Routes>
-    
-    <Footer/>
-    </BrowserRouter>
+      <Router>
+        <Navbar />
+        <Routes>
+         <Route path="/"element={<Home />}/>
+          <Route path="/home" element={<Home setResults={setResults} results={results} />} />
+        <Route path="/cars" element={<Cars results={results} />} />
+      
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+        
+          <Route path="/bookings" element={<BookingLayout />} />
+          <Route path="/settings" element={<SettingsLayout />} />
+          <Route path="/dashboard" element={<DashboardLayout />} />
+          <Route path="/addcar" element={<AddCarLayout />} />
+          <Route path="/adminSignup" element={<AdminSignup />} />
+          <Route path="/adminLogin" element={<AdminLogin />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
-
-    
 
 const BookingLayout = () => {
   return (
@@ -53,6 +64,7 @@ const SettingsLayout = () => {
 };
 
 const DashboardLayout = () => {
+
   return (
     <>
       <Sidebar />
